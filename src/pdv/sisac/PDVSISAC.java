538266edecbb0b2a85b2efc4b272bfc129a1376f
meirinaldo <meirinaldo@Meirinaldo-Junior>
2016-02-10 22:29:48 -0300

    Implementação modulo PDV

---
+++ src/pdv/sisac/PDVSISAC.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pdv.sisac;
+
+
+public class PDVSISAC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //abrindo tela do PDV
+        
+        pdv frame_pdv = new pdv();
+        frame_pdv.setVisible(true);
+    }
+    
+}
